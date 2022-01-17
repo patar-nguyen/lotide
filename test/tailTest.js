a57218353a3f01f39e9b2caec07b5b2f77479d62
Patrick Nguyen <patar.nguyen@gmail.com>
2022-01-17 17:03:36 -0500

    Lotide Assignment

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,22 @@
 const tail = require('../tail');
-
 const assertEqual = require('../assertEqual');
 
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+describe('#tail', () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-assertEqual(result, ["Lighthouse", "Labs"]);
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
