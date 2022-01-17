24d015581d673ad1e2f9e3b2e9e497b7490e7973
Patrick Nguyen <patar.nguyen@gmail.com>
2022-01-17 15:03:56 -0500

    Lotide Assignment

--- tail.js
+++ tail.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function(array) {
   let tail = [];
 
@@ -7,17 +9,12 @@ const tail = function(array) {
   return tail;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = tail;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
